b51eff9531d1e6df1dc618a74892012e5791db3f
chanochan <75952111+chanochan@users.noreply.github.com>
2021-07-11 04:31:01 +0430

    Fix null safety of onDone function

--- lib/pin_code_text_field.dart
+++ lib/pin_code_text_field.dart
@@ -126,7 +126,7 @@ class PinCodeTextField extends StatefulWidget {
   final TextStyle pinTextStyle;
   final double pinBoxHeight;
   final double pinBoxWidth;
-  final OnDone onDone;
+  final OnDone? onDone;
   final bool hasError;
   final Color errorBorderColor;
   final Color hasTextBorderColor;
@@ -450,7 +450,7 @@ class PinCodeTextFieldState extends State<PinCodeTextField>
     });
     if (text.length == widget.maxLength) {
       FocusScope.of(context).requestFocus(FocusNode());
-      widget.onDone(text);
+      widget.onDone?.call(text);
     }
   }
 
